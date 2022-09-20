8582d07b3e35d23cb441a813ea07a57513cbfab6
thomasmelo <thomas.melo@terra.com.br>
2022-09-20 10:43:45 -0300

    Criação das Models

--- app/Models/Acessorio.php
+++ app/Models/Acessorio.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+#Models
+use App\Models\{AcessorioModelo};
 
 class Acessorio extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'acessorios';
+    protected $primaryKey = 'id_acessorio';
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+    protected $fillable = ['acessorio', 'descricao'];
+
+    /*
+    |------------------------------------------------
+    | Relacionamentos
+    | https://laravel.com/docs/9.x/eloquent-relationships
+    |------------------------------------------------    
+    */
+
+    public function modelos()
+    {
+        return $this->belongsTo(AcessorioModelo::class, 'id_acessorio', 'id_acessorio');
+    }
+
 }
--- app/Models/Veiculo.php
+++ app/Models/Veiculo.php
@@ -4,8 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+#Models
+use App\Models\{Modelo, User};
 
 class Veiculo extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'veiculos';
+    protected $primaryKey = 'id_veiculo';
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+    protected $fillable = [
+                        'id_modelo',  
+                        'id_user', 
+                        'valor', 
+                        'cor', 
+                        'placa', 
+                        'descricao'
+                        ];
+
+    /*
+    |------------------------------------------------
+    | Relacionamentos
+    | https://laravel.com/docs/9.x/eloquent-relationships
+    |------------------------------------------------    
+    */
+
+    public function modelo()
+    {
+        return $this->belongsTo(Modelo::class, 'id_modelo', 'id_modelo');
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id_user');
+    }
 }
